2b4d736fda86bb69956422f4d48bde64a82d29af
Wilberth Calan <wilberth_calan@outlook.com>
2023-09-06 10:39:25 -0600

    Final

--- script.js
+++ script.js
@@ -27,49 +27,65 @@
   }*/
 
 /*Iniciador de la página con la información*/
-request(0)
+request()
 var idR = 0
 var searchValue = ''
+var pokemons = []
 
 /*Función de solicitud al json*/
-function request(action) {
+function request() {
   fetch("./info.json").then((value) => {
     return value.json()
   }).then((valueJSON) => {
-    switch (action) {
-      case 0:
-        createCards(valueJSON)
-        break
-      case 1:
-        requestFill(valueJSON)
-        break
-      case 2:
-        search(valueJSON)
-        break
-    }
-  }).catch((error)=>{
+    createPokes(valueJSON)
+  }).catch((error) => {
     alert(`${error}`)
   })
 }
 
+function createPokes(pokes) {
+  for (i = 0; i < pokes.length; i++) {
+    let abilities1 = abilitiesUpper(pokes[i])
+    let types = typesUpper(pokes[i])
+    let weakness1 = weaknessUpper(pokes[i])
+    let poke = new Pokemon({
+      abilities: abilities1,
+      detailPageURL: pokes[i].detailPageURL,
+      weight: pokes[i].weight,
+      weakness: weakness1,
+      number: pokes[i].number,
+      height: pokes[i].height,
+      collectibles_slug: pokes[i].collectibles_slug,
+      featured: pokes[i].featured,
+      slug: pokes[i].slug,
+      name: pokes[i].name,
+      ThumbnailAltText: pokes[i].ThumbnailAltText,
+      ThumbnailImage: pokes[i].ThumbnailImage,
+      id: pokes[i].id,
+      type: types
+    })
+    pokemons.push(poke)
+  }
+  createCards()
+}
 
-class Pokemon{
-    constructor(abilities, detailPageURL, weight, weakness, number, height, collectibles_slug, featured, slug, name, ThumbnailAltText, ThumbnailImage, id, type){
-        this.abilities = abilities
-        this.detailPageURL = detailPageURL
-        this.weight = weight
-        this.weakness = weakness
-        this.number = number
-        this.height = height
-        this.collectibles_slug = collectibles_slug
-        this.featured = featured
-        this.slug = slug
-        this.name = name
-        this.ThumbnailAltText = ThumbnailAltText
-        this.ThumbnailImage = ThumbnailImage
-        this.id = id
-        this.type = type
-    }
+class Pokemon {
+  constructor({ abilities, detailPageURL, weight, weakness, number, height, collectibles_slug, featured, slug, name, ThumbnailAltText, ThumbnailImage, id, type }) {
+    this.abilities = abilities
+    this.detailPageURL = detailPageURL
+    this.weight = weight
+    this.weakness = weakness
+    this.number = number
+    this.height = height
+    this.collectibles_slug = collectibles_slug
+    this.featured = featured
+    this.slug = slug
+    this.name = name
+    this.ThumbnailAltText = ThumbnailAltText
+    this.ThumbnailImage = ThumbnailImage
+    this.id = id
+    this.type = type
+  }
 }
 
 /* Bloque de funciones para capitalizar las habilidades, tipos y debilidades */
@@ -83,6 +99,7 @@ function abilitiesUpper(pokemon) {
     }
     i++
   }
+
   return abilities
 }
 
@@ -113,12 +130,10 @@ function weaknessUpper(pokemon) {
 }
 
 /* Función para la creación de cada tarjeta de los pokemones contenidos en el json  */
-function createCards(pokemons) {
+function createCards() {
   let i = 0
   document.getElementById("cards").innerHTML = "";
   for (let pokemon of pokemons) {
-    let abilities = abilitiesUpper(pokemon)
-    let types = typesUpper(pokemon)
     document.getElementById("cards").innerHTML += `
     <div class="col">
       <div class="card h-100">
@@ -126,8 +141,8 @@ function createCards(pokemons) {
           <img src="${pokemon.ThumbnailImage}"
             class="card-img-top img-fluid img-thumbnail" alt="${pokemon.ThumbnailAltText}" id="pokemonThumbnail">
         <div class="card-body">
-          <h5 class="card-title">Type: <small class="text-body-secondary" id="pokemonTypes">${types}</small></h5>
-          <h5 class="card-title">Abilities: <small class="text-body-secondary" id="pokemonAbilities">${abilities}</small></h5>          
+          <h5 class="card-title">Type: <small class="text-body-secondary" id="pokemonTypes">${pokemon.type}</small></h5>
+          <h5 class="card-title">Abilities: <small class="text-body-secondary" id="pokemonAbilities">${pokemon.abilities}</small></h5>          
         </div>
         <div class="card-footer">
           <button type="button" class="btn btn-primary" data-bs-toggle="modal"
@@ -144,33 +159,35 @@ function createCards(pokemons) {
 }
 
 /*Función que se lanza al darle clik al botón de buscar */
-function searchButton() {    
+function searchButton() {
   searchValue = document.getElementById('searchInput').value
-  document.getElementById('searchInput').value = '' 
-  request(2)
+  document.getElementById('searchInput').value = ''
+  if (searchValue == '') {
+    createCards()
+  } else {
+    search()
+  }
 }
 /*Función que detecta si dentro del contenedor del input se presiona Enter */
 var searchInput = document.getElementById('searchInput');
-searchInput.addEventListener('keyup', function(e) {  
+searchInput.addEventListener('keyup', function (e) {
   if (e.key === 'Enter') {
     searchButton()
   }
 })
 
 /*Función para "recargar" la página al presionar ESC */
-document.addEventListener("keyup", function(event) {
+document.addEventListener("keyup", function (event) {
   if (event.key === 'Escape') {
-      request(0)
+    createCards()
   }
 });
 
 /* Función para generar las tarjetas de los pokemones encontrados */
-function search(pokemons) {
+function search() {
   document.getElementById("cards").innerHTML = "";
   for (let pokemon of pokemons) {
     if (pokemon.name.includes(searchValue) || pokemon.number.includes(searchValue)) {
-      let abilities = abilitiesUpper(pokemon)
-      let types = typesUpper(pokemon)
       document.getElementById("cards").innerHTML += `
     <div class="col">
       <div class="card h-100">
@@ -178,8 +195,8 @@ function search(pokemons) {
           <img src="${pokemon.ThumbnailImage}"
             class="card-img-top img-fluid img-thumbnail" alt="${pokemon.ThumbnailAltText}" id="pokemonThumbnail">
         <div class="card-body">
-          <h5 class="card-title">Type: <small class="text-body-secondary" id="pokemonTypes">${types}</small></h5>
-          <h5 class="card-title">Abilities: <small class="text-body-secondary" id="pokemonAbilities">${abilities}</small></h5>          
+          <h5 class="card-title">Type: <small class="text-body-secondary" id="pokemonTypes">${pokemon.type}</small></h5>
+          <h5 class="card-title">Abilities: <small class="text-body-secondary" id="pokemonAbilities">${pokemon.abilities}</small></h5>          
         </div>
         <div class="card-footer">
           <button type="button" class="btn btn-primary" data-bs-toggle="modal"
@@ -194,23 +211,20 @@ function search(pokemons) {
 /* Función que se lanza al darle al botón About generar */
 function modalFill(id) {
   idR = id
-  request(1)
+  requestFill()
 }
 
 /* Función para el rellenado del modal */
-function requestFill(pokemons) {
+function requestFill() {
   for (let pokemon of pokemons) {
     if (pokemon.id == idR) {
       document.getElementById('modalNumberName').innerHTML = `Nº ${pokemon.number}<span class="strong"> ${pokemon.name} </span> `
       let imgThumb = document.getElementById('modalThumbnail')
       imgThumb.setAttribute('src', pokemon.ThumbnailImage)
       imgThumb.setAttribute('alt', pokemon.ThumbnailAltText)
-      let types = typesUpper(pokemon)
-      document.getElementById('modalType').innerHTML = `&nbsp &nbsp &nbsp ${types}`
-      let abilities = abilitiesUpper(pokemon)
-      document.getElementById('modalAbilities').innerHTML = `&nbsp &nbsp &nbsp${abilities}`
-      let weakness = weaknessUpper(pokemon)
-      document.getElementById('modalWeakness').innerHTML = `&nbsp &nbsp${weakness}`
+      document.getElementById('modalType').innerHTML = `&nbsp &nbsp &nbsp ${pokemon.type}`
+      document.getElementById('modalAbilities').innerHTML = `&nbsp &nbsp &nbsp${pokemon.abilities}`
+      document.getElementById('modalWeakness').innerHTML = `&nbsp &nbsp${pokemon.weakness}`
       document.getElementById('modalHeight').innerHTML = `${pokemon.height}`
       document.getElementById('modalWeight').innerHTML = `${pokemon.weight}`
       let url = document.getElementById('url')
